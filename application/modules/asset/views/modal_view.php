33904d2acf1c93937c6a9a3623c9ebc8a5df4e4b
azizun3 <azizun3@github.com>
2025-10-26 14:16:34 +0700

    asset

---
+++ application/modules/asset/views/modal_view.php
@@ -0,0 +1,91 @@
+<?php
+
+$qData	= "SELECT * FROM asset WHERE id='" . $this->uri->segment(3) . "'";
+$dataD	= $this->db->query($qData)->result_array();
+$list_dept = $this->db->get_where('ms_department', ['deleted_by' => null])->result_array();
+$list_catg = $this->Asset_model->getList('asset_category');
+
+// $QUERY	 	= "SELECT * FROM ms_costcenter WHERE id_costcenter = '".$dataD[0]['cost_center']."' ORDER BY nama_costcenter ASC";
+// $costcenter	= $this->db->query($QUERY)->row();
+?>
+
+<div class="box box-primary">
+	<div class="box-header">
+		<h3 class="box-title"></h3>
+	</div>
+	<div class="box-body">
+		<div class='form-group row'>
+			<label class='label-control col-sm-2'><b>Nama Asset <span class='text-red'>*</span></b></label>
+			<div class='col-sm-4'>
+				<?php
+				echo form_input(array('id' => 'nm_asset', 'name' => 'nm_asset', 'class' => 'form-control input-md', 'autocomplete' => 'off', 'placeholder' => 'Nama Asset', 'readonly' => 'readonly'), strtoupper($dataD[0]['nm_asset']));
+				echo form_input(array('type' => 'hidden', 'id' => 'id', 'name' => 'id'), $dataD[0]['id']);
+				echo form_input(array('type' => 'hidden', 'id' => 'kd_asset', 'name' => 'kd_asset'), $dataD[0]['kd_asset']);
+				echo form_input(array('type' => 'hidden', 'id' => 'helpa', 'name' => 'helpa', 'value' => 'N'));
+				?>
+			</div>
+			<label class='label-control col-sm-2'><b>Kategori <span class='text-red'>*</span></b></label>
+			<div class='col-sm-4'>
+				<select name='category' id='category' class='form-control input-md' disabled>
+					<?php
+					foreach ($list_catg as $val => $valx) {
+						$selx = ($dataD[0]['category'] == $valx['id']) ? 'selected' : '';
+						echo "<option value='" . $valx['id'] . "' " . $selx . ">" . strtoupper($valx['nm_category']) . "</option>";
+					}
+					?>
+				</select>
+			</div>
+		</div>
+		<div class='form-group row'>
+			<label class='label-control col-sm-2'><b>Outlet <span class='text-red'>*</span></b></label>
+			<div class='col-sm-4'>
+				<select name='outlet' id='outlet' class='form-control input-md chosen-select' disabled>
+					<option value='0'>Pilih Outlet</option>
+					<?php
+					foreach ($list_dept as $val => $valx) {
+						$selected	= ($valx['id'] == $dataD[0]['outlet'] )?'selected':'';
+						echo "<option value='" . $valx['id'] . "' " . $selected . ">" . strtoupper($valx['nama']) . "</option>";
+					}
+					?>
+				</select>
+			</div>
+			<label  class='label-control col-sm-2'><b>Merk <span class='text-red'>*</span></b></label>
+			<div class='col-sm-4' >
+				<input type="text" id="merk" name="merk" value="<?=$dataD[0]['merk']?>" class="form-control input-md" autocomplete="off" placeholder="Merk" readonly>
+			</div>
+		</div>
+		<div class='form-group row'>
+			<label class='label-control col-sm-2'><b>Nilai Asset <span class='text-red'>*</span></b></label>
+			<div class='col-sm-4'>
+				<?php
+				echo form_input(array('id' => 'nilai_asset', 'name' => 'nilai_asset', 'class' => 'form-control input-md', 'autocomplete' => 'off', 'placeholder' => 'Nilai Asset', 'data-decimal' => '.', 'data-thousand' => '', 'data-precision' => '0', 'data-allow-zero' => false, 'readonly' => 'readonly'), number_format($dataD[0]['nilai_asset']));
+				?>
+			</div>
+			<label class='label-control col-sm-2'><b>Dipresiasi Perbulan</b></label>
+			<div class='col-sm-4'>
+				<?php
+				echo form_input(array('id' => 'value', 'name' => 'value', 'class' => 'form-control input-md', 'autocomplete' => 'off', 'placeholder' => 'Dipresiasi Perbulan', 'readonly' => 'readonly', 'data-decimal' => '.', 'data-thousand' => '', 'data-precision' => '0', 'data-allow-zero' => false), number_format($dataD[0]['value']));
+				?>
+			</div>
+		</div>
+		<div class='form-group row'>
+			<label class='label-control col-sm-2'><b>Lokasi <span class='text-red'>*</span></b></label>
+			<div class='col-sm-4'>
+				<input type="text" id="qty" name="qty" class="hide" value="1" >
+				<input type="text" id="lokasi_asset" name="lokasi_asset" value="<?=$dataD[0]['lokasi_asset']?>" class="form-control input-md" autocomplete="off" placeholder="Lokasi" disabled>
+
+			</div> 
+			<label class='label-control col-sm-2'><b>Jangka Waktu <span class='text-red'>*</span></b></label>
+			<div class='col-sm-4'>
+				<select name='depresiasi' id='depresiasi' class='form-control input-md' disabled>
+					<?php
+					for ($a = 1; $a <= 8; $a++) {
+						$selx = ($dataD[0]['depresiasi'] == $a) ? 'selected' : '';
+						echo "<option value='" . $a . "' " . $selx . ">" . $a . " Tahun</option>";
+					}
+					?>
+				</select>
+			</div>
+		</div>
+	</div>
+</div>
