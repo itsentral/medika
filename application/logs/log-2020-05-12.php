4bcbc226b73386177562cc82f0704aba22f58579
hikmat20 <hikmataulia20@gmail.com>
2025-10-28 02:49:31 +0700

    update layanan

--- application/logs/log-2020-05-12.php
+++
@@ -1,6 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2020-05-12 09:40:36 --> Severity: Notice --> Undefined variable: nama_program D:\xampp1\htdocs\medika\application\modules\users\views\login_animate.php 6
-ERROR - 2020-05-12 09:40:36 --> 404 Page Not Found: /index
-ERROR - 2020-05-12 09:40:47 --> Severity: Notice --> Undefined variable: nama_program D:\xampp1\htdocs\medika\application\modules\users\views\login_animate.php 6
-ERROR - 2020-05-12 09:40:47 --> 404 Page Not Found: /index
