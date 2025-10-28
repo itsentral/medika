a2c1ac242bd4aa4ae1c289ca8b571b9aeec2a2a3
Alief Daffa <aliefdaffa572@gmail.com>
2025-10-28 11:29:24 +0700

    Development - Pemeriksaan

--- application/modules/pemeriksaan/views/add_pemeriksaan.php
+++ application/modules/pemeriksaan/views/add_pemeriksaan.php
@@ -1,6 +1,7 @@
 <?php
-$id_kategori = (isset($header) && !empty($header)) ? $header->id_kategori : '';
-$nm_kelompok = (isset($header) && !empty($header)) ? $header->nm_kelompok : '';
+$id_kategori = (isset($data_pemeriksaan) && !empty($data_pemeriksaan)) ? $data_pemeriksaan->id_kategori : '';
+$nm_kelompok = (isset($data_pemeriksaan) && !empty($data_pemeriksaan)) ? $data_pemeriksaan->nm_kelompok_pemeriksaan : '';
+$nm_pemeriksaan = (isset($data_pemeriksaan) && !empty($data_pemeriksaan)) ? $data_pemeriksaan->nm_pemeriksaan : '';
 ?>
 <link href="<?= base_url('assets/css/chosen.min.css') ?>" rel="stylesheet" />
 <input type="hidden" name="id" value="<?= $id ?>">
@@ -28,13 +29,24 @@ $nm_kelompok = (isset($header) && !empty($header)) ? $header->nm_kelompok : '';
             <label for="">Nama Kelompok Pemeriksaan <span class="text-danger">*</span></label>
             <select name="kelompok_pemeriksaan" class="form-control form-control-sm select2" required>
                 <option value="">- Choose Kelompok Pemeriksaan -</option>
+                <?php
+                if (isset($list_kelompok_pemeriksaan)) {
+                    foreach ($list_kelompok_pemeriksaan as $item) {
+                        $selected = '';
+                        if ($item->id == $data_pemeriksaan->id_kelompok_pemeriksaan) {
+                            $selected = 'selected';
+                        }
+                        echo '<option value="' . $item->id . '" ' . $selected . '>' . $item->nm_kelompok . '</option>';
+                    }
+                }
+                ?>
             </select>
         </div>
     </div>
     <div class="col-md-6">
         <div class="form-group">
             <label for="">Pemeriksaan <span class="text-danger">*</span></label>
-            <input type="text" name="pemeriksaan" id="" class="form-control form-control-sm" required>
+            <input type="text" name="pemeriksaan" id="" class="form-control form-control-sm" value="<?= $nm_pemeriksaan ?>" required>
         </div>
     </div>
 </div>
